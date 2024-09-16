47ff95f326f445ad1005c1559f80878112e227bf
Chen <101056148+prok0801@users.noreply.github.com>
2024-09-16 16:53:12 +0700

    commander_

--- commander.py
+++ commander.py
@@ -1,67 +1,75 @@
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-def var(i, j, n):
-    return i * n + j + 1
-
 def add_commander_encoding(cnf, variables, group_size):
     if len(variables) <= 1:
         return
-    groups = [variables[i:i + group_size] for i in range(0, len(variables), group_size)]
+
     commander_vars = []
-    
-    for group in groups:
+    for i in range(0, len(variables), group_size):
+        group = variables[i:i + group_size]
         if len(group) > 1:
-            commander_var = max(var for clause in cnf for var in clause) + 1
+            commander_var = cnf.nv + 1
+            cnf.nv += 1  
             commander_vars.append(commander_var)
-            cnf.append(group + [-commander_var])
-            
-            for i in range(len(group)):
-                for j in range(i + 1, len(group)):
-                    cnf.append([-group[i], -group[j]])
-                    
+
             for var in group:
-                cnf.append([-commander_var, var])
-                
-    if commander_vars:
+                cnf.append([-var, commander_var])
+
+            for j in range(len(group)):
+                for k in range(j + 1, len(group)):
+                    cnf.append([-group[j], -group[k]])
+
+    if len(commander_vars) > 1:
         add_commander_encoding(cnf, commander_vars, group_size)
 
-def add_exactly_one(cnf, variables, group_size):
-    cnf.append(variables)
-    add_commander_encoding(cnf, variables, group_size)
-
-def generate_clauses(n, group_size):
-    cnf = CNF()
-    for i in range(n):
-        row_vars = [var(i, j, n) for j in range(n)]
-        col_vars = [var(j, i, n) for j in range(n)]
-        add_exactly_one(cnf, row_vars, group_size)
-        add_exactly_one(cnf, col_vars, group_size)  
-
-    for d in range(2 * n - 1):
-        main_diag_vars = [var(i, d - i, n) for i in range(n) if 0 <= d - i < n]
-        anti_diag_vars = [var(i, i - d + n - 1, n) for i in range(n) if 0 <= i - d + n - 1 < n]
-        add_commander_encoding(cnf, main_diag_vars, group_size)
-        add_commander_encoding(cnf, anti_diag_vars, group_size)
-
-    return cnf
 
 def solve_n_queens(n, group_size):
-    cnf = generate_clauses(n, group_size)
+    cnf = CNF()
+    cnf.nv = 0 
+    variables = []
 
-    with Solver(bootstrap_with=cnf) as solver:
+    for i in range(n):
+        row_vars = []
+        for j in range(n):
+            var = cnf.nv + 1
+            cnf.nv += 1
+            row_vars.append(var)
+        variables.append(row_vars)
+
+    for i in range(n):
+        cnf.append(variables[i])  # ALO
+        add_commander_encoding(cnf, variables[i], group_size)  # AMO
+
+        col_vars = [variables[j][i] for j in range(n)]
+        cnf.append(col_vars)  # ALO
+        add_commander_encoding(cnf, col_vars, group_size)  # AMO
+
+    for d in range(2 * n - 1):
+        diag1_vars = [variables[i][d - i] for i in range(max(0, d - n + 1), min(n, d + 1))]
+        if len(diag1_vars) > 1:
+            add_commander_encoding(cnf, diag1_vars, group_size)
+
+        diag2_vars = [variables[i][n - d + i - 1] for i in range(max(0, d - n + 1), min(n, d + 1))]
+        if len(diag2_vars) > 1:
+            add_commander_encoding(cnf, diag2_vars, group_size)
+
+    with Solver(bootstrap_with=cnf.clauses) as solver:
         if solver.solve():
             model = solver.get_model()
-            return format_solution(model, n)
-        return None
+            solution = []
+            for i in range(n):
+                row = ""
+                for j in range(n):
+                    if model[variables[i][j] - 1] > 0:
+                        row += "Q"
+                    else:
+                        row += "."
+                solution.append(row)
+            return solution
+        else:
+            return None
 
-def format_solution(model, n):
-    board = [['.'] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if var(i, j, n) in model:
-                board[i][j] = 'Q'
-    return [' '.join(row) for row in board]
 
 def print_solution(solution):
     if solution:
@@ -70,8 +78,9 @@ def print_solution(solution):
     else:
         print("No solution found.")
 
+
 if __name__ == "__main__":
-    n = 8 
-    group_size = 3 
+    n = 16
+    group_size = 3
     solution = solve_n_queens(n, group_size)
     print_solution(solution)
