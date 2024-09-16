8b709618c607ba8cc51519d60f1073940dbc1464
Chen <101056148+prok0801@users.noreply.github.com>
2024-09-16 13:21:55 +0700

    Update commander.py

--- commander.py
+++ commander.py
@@ -1,95 +1,77 @@
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-new_variables_count = 0
-
 def var(i, j, n):
     return i * n + j + 1
 
-def generate_new_variables(length):
-    global new_variables_count
-    start = new_variables_count + 1
-    new_variables_count += length
-    return [i for i in range(start, start + length)]
-
-def at_most_k(cnf, variables, k):
-    if k >= len(variables):
+def add_commander_encoding(cnf, variables, group_size):
+    if len(variables) <= 1:
         return
-
-    if k == 1:
-        at_most_one(cnf, variables)
-        return
-
-    chunk_size = k + 1
-    groups = [variables[i:i + chunk_size] for i in range(0, len(variables), chunk_size)]
+    groups = [variables[i:i + group_size] for i in range(0, len(variables), group_size)]
+    commander_vars = []
     
-    commanders = []
     for group in groups:
         if len(group) > 1:
-            commander = generate_new_variables(1)[0]
-            commanders.append(commander)
+            commander_var = max(var for clause in cnf for var in clause) + 1
+            commander_vars.append(commander_var)
+            cnf.append(group + [-commander_var])
+            
+            for i in range(len(group)):
+                for j in range(i + 1, len(group)):
+                    cnf.append([-group[i], -group[j]])
+                    
             for var in group:
-                cnf.append([-var, commander])
-            at_most_one(cnf, group)
-    
+                cnf.append([-commander_var, var])
+                
+    if commander_vars:
+        add_commander_encoding(cnf, commander_vars, group_size)
 
-    at_most_k(cnf, commanders, k)
-
-def at_most_one(cnf, variables):
-    for i in range(len(variables)):
-        for j in range(i + 1, len(variables)):
-            cnf.append([-variables[i], -variables[j]])
-
-def exactly_one(cnf, variables):
+def add_exactly_one(cnf, variables, group_size):
     cnf.append(variables)
-    at_most_one(cnf, variables)
+    add_commander_encoding(cnf, variables, group_size)
 
-def generate_clauses(n):
+def generate_clauses(n, group_size):
     cnf = CNF()
-
     for i in range(n):
         row_vars = [var(i, j, n) for j in range(n)]
-        exactly_one(cnf, row_vars)
-
-    for j in range(n):
-        col_vars = [var(i, j, n) for i in range(n)]
-        exactly_one(cnf, col_vars)
+        col_vars = [var(j, i, n) for j in range(n)]
+        add_exactly_one(cnf, row_vars, group_size)
+        add_exactly_one(cnf, col_vars, group_size)  
 
     for d in range(2 * n - 1):
         main_diag_vars = [var(i, d - i, n) for i in range(n) if 0 <= d - i < n]
-        at_most_one(cnf, main_diag_vars)
-
-    for d in range(2 * n - 1):
         anti_diag_vars = [var(i, i - d + n - 1, n) for i in range(n) if 0 <= i - d + n - 1 < n]
-        at_most_one(cnf, anti_diag_vars)
+        add_commander_encoding(cnf, main_diag_vars, group_size)
+        add_commander_encoding(cnf, anti_diag_vars, group_size)
 
     return cnf
 
-def solve_n_queens(n):
-    cnf = generate_clauses(n)
+def solve_n_queens(n, group_size):
+    cnf = generate_clauses(n, group_size)
 
     with Solver(bootstrap_with=cnf) as solver:
         if solver.solve():
             model = solver.get_model()
-            solution = []
-            for i in range(n):
-                row = ['.'] * n
-                for j in range(n):
-                    if var(i, j, n) in model:
-                        row[j] = 'Q'
-                solution.append(' '.join(row))
-            return solution
-        else:
-            return None
+            return format_solution(model, n)
+        return None
+
+def format_solution(model, n):
+    board = [['.'] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if var(i, j, n) in model:
+                board[i][j] = 'Q'
+    return [' '.join(row) for row in board]
 
 def print_solution(solution):
-    if solution is None:
-        print("No solution found.")
-    else:
+    if solution:
         for row in solution:
             print(row)
+    else:
+        print("No solution found.")
 
 if __name__ == "__main__":
-    n = 8
-    solution = solve_n_queens(n)
+    n = 8 
+    group_size = 3 
+    solution = solve_n_queens(n, group_size)
     print_solution(solution)
